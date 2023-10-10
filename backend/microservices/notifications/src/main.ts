3a0ba08e0ff8a4597d1da44131ca0b7ccaaa275b
manuelquimis <e1316360716@live.uleam.edu.ec>
2023-10-09 22:56:46 -0500

    docker-compose

--- backend/microservices/notifications/src/main.ts
+++ backend/microservices/notifications/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  await app.listen(parseInt(process.env.PORT));
+
+  // await app.listen(3000);
 }
 bootstrap();
